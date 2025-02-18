8b755f8de4668eba26db62a54f064cdbcc577ce4
Davox14 <davide.re02@universitadipavia.it>
2025-02-19 00:01:49 +0100

    registration to view

--- PROGETTO_G25/src/it/unipv/ingsfw/bitebyte/controller/Authcontroller.java
+++ PROGETTO_G25/src/it/unipv/ingsfw/bitebyte/controller/Authcontroller.java
@@ -1,3 +1,4 @@
+//CONTROLLER
 package it.unipv.ingsfw.bitebyte.controller;
 import it.unipv.ingsfw.bitebyte.dao.ClienteDAO;
 
@@ -11,8 +12,13 @@ import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage; 
+import javafx.fxml.Initializable;
 
-public class Authcontroller {
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Authcontroller implements Initializable{
 
 	   	private ClienteDAO clienteDAO = new ClienteDAO();
 
@@ -39,16 +45,31 @@ public class Authcontroller {
 	    @FXML private Button pulsanteVaiARegistrazione;
 	    @FXML private Button pulsanteVaiALogin;
 
+	    
+	    
+	    
+	    
+	    
+	    @Override
+	    public void initialize(URL location, ResourceBundle resources) {
+	        // Questo metodo verrà chiamato quando la vista è completamente caricata
+	        System.out.println("Metodo initialize chiamato!");
+	    }
+	
 	    @FXML
-	    public void inizializza() {
-	        // Controllo in tempo reale mentre l'utente digita
-	    	emailReg.setOnKeyReleased(this::controllaEmail);
-	    	passwordReg.setOnKeyReleased(this::controllaPassword);
-	    	
-	    	pulsanteVaiARegistrazione.setOnAction(event -> switchScene("registration-view.fxml", "Registrazione"));
-	    	pulsanteRegistrati.setOnAction(event -> controlloCampi());
+	    public void cambiaScena(javafx.event.ActionEvent event) {
+	        Button clickedButton = (Button) event.getSource();
+	        Stage stage = (Stage) clickedButton.getScene().getWindow();  // Ottieni lo Stage dal bottone premuto
+	        // Verifica quale bottone è stato premuto e cambia scena di conseguenza
+	        if (clickedButton.getId().equals("pulsanteVaiALogin")) {
+	        	System.out.println("sono in switch scene");
+	            switchScene(stage, "login-view.fxml", "Login");
+	        } else if (clickedButton.getId().equals("pulsanteRegistrati")) {
+	            switchScene(stage, "registration-view.fxml", "Registrazione");
+	        }
 	    }
 
+	    
 	    @FXML
 	    public void accedi() {
 	        String nomeUtente = usernameLogin.getText();
@@ -170,13 +191,33 @@ public class Authcontroller {
 	        clienteDAO.registraCliente(nuovoCliente);
 	        showAlert("Successo", "REGISTRAZIONE COMPLETATA!");
 	        
-	        switchScene("login-view.fxml", "Login");
+	        //switchScene("login-view.fxml", "Login");
 	        	        
 	    }
 	    
-	    private void switchScene(String fxml, String title) {
+	    
+	    
+	    private void switchScene(Stage stage, String fxml, String title) {
 	        try {
-	            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+	        	System.out.println("sono in switch scene");
+	            FXMLLoader loader = new FXMLLoader(getClass().getResource("/" + fxml));
+	            Parent root = loader.load();
+	            stage.setTitle(title);
+	            stage.setScene(new Scene(root));
+	            stage.show();
+	        } catch (Exception e) {
+	            e.printStackTrace();  // Questo mostrerà eventuali errori
+	        }
+	    }
+	    
+	    
+	    
+	    
+	    /*
+	    private void switchScene(String fxml, String title) {
+	    	System.out.println("/" + fxml);
+	        try {
+	            FXMLLoader loader = new FXMLLoader(getClass().getResource("/" + fxml));
 	            Parent root = loader.load();
 	            Stage stage = (Stage) pulsanteAccedi.getScene().getWindow();
 	            stage.setTitle(title);
@@ -187,7 +228,7 @@ public class Authcontroller {
 	        }
 	    }
 	    
-	    
+	*/    
 	    
 	
 }
--- PROGETTO_G25/src/it/unipv/ingsfw/bitebyte/view/Main1.java
+++ PROGETTO_G25/src/it/unipv/ingsfw/bitebyte/view/Main1.java
@@ -11,7 +11,7 @@ public class Main1 extends Application {
 	    public void start(Stage primaryStage) throws Exception {
 	        Parent root = FXMLLoader.load(getClass().getResource("/registration-view.fxml"));
 	        primaryStage.setTitle("Login");
-	        primaryStage.setScene(new Scene(root, 400, 293));
+	        primaryStage.setScene(new Scene(root, 600, 400));
 	        primaryStage.show();
 	    }
 
